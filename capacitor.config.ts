13c8baf2a0834c7a14935a837191c98523292928
David Guentelican <david.guentelican@gmail.com>
2024-11-11 11:05:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-ecd-asistencia',
+  webDir: 'www'
+};
+
+export default config;
