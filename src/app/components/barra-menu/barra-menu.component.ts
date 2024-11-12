26e2a30a2d863db38b4c202d5dfc1b59b8fe2def
David Guentelican <david.guentelican@gmail.com>
2024-11-12 00:52:02 -0300

    se implementa correctamente la navegacion entre pantallas desde el menu lateral del componente barra-menu

--- src/app/components/barra-menu/barra-menu.component.ts
+++ src/app/components/barra-menu/barra-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -10,15 +11,63 @@ export class BarraMenuComponent  implements OnInit {
 
   //variables dinamicas del componente
   @Input() titulo: string = '';
+  //variable para controlar la visibilidad de los botones segun la pantalla donde este
+  rutaActual: string = '';
 
   //inyectar dependencias
-  constructor(private menuCtrl: MenuController) { }
+  constructor(private menuCtrl: MenuController, private router: Router) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    //detecta la ruta al cargar el componente
+    this.router.events.subscribe(() => {
+      this.rutaActual = this.router.url;
+    });
+  }
 
   //abrir el menu lateral
   abrirMenu() {
-    this.menuCtrl.open('end');
+    this.menuCtrl.open('menu-end');
+  }
+
+  //navegar al principal
+  principal() {
+    let extras: NavigationExtras = {
+      replaceUrl: true
+    }
+
+    this.router.navigate(['principal'], extras);
+  }
+
+  //navegar a actualizacion de datos
+  actualizacionDatos() {
+    let extras: NavigationExtras = {
+      replaceUrl: true
+    }
+
+    this.router.navigate(['actualizacion-datos'], extras);
+  }
+
+  //navegar a sedes
+  sedes() {
+    let extras: NavigationExtras = {
+      replaceUrl: true
+    }
+
+    this.router.navigate(['sedes'], extras);
+  }
+
+  //navegar a asistencia
+  asistencia() {
+    let extras: NavigationExtras = {
+      replaceUrl: true
+    }
+
+    this.router.navigate(['asistencia'], extras);
+  }
+
+  //metodo para verificar si la ruta actual es la misma
+  esLaRutaActual(ruta: string): boolean {
+    return this.rutaActual.includes(ruta);
   }
 
 }
