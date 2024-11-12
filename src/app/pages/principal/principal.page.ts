31301d6b1b0bc1150e526ca44803c1af189af6f3
David Guentelican <david.guentelican@gmail.com>
2024-11-12 12:43:09 -0300

    integrados cards a principal con diseño para datos de usuario y asignaturas, con skeletons de carga

--- src/app/pages/principal/principal.page.ts
+++ src/app/pages/principal/principal.page.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalPage implements OnInit {
 
+  /* VARIABLES --------------------------------------------------------------------------------- */
+
+  //skeletons
+  skeletonsCargando: boolean = true;
+  //variables para mostrar usuario
+  correo: string = '';
+  nombre: string = '';
+  apellido: string = '';
+  carrera: string = '';
+
+
+  /* CONSTRUCTOR --------------------------------------------------------------------------------- */
+
   constructor() { }
 
+
+  /* ngOnInit --------------------------------------------------------------------------------- */
+
   ngOnInit() {
+    //PARA LOS SKELETONS
+    this.skeletonsCargando = true;
+
+    // await this.mostrarUsuarioLogueado(); //mostrar usuario logueado guardado en db
+
+    setTimeout(async () => {
+      this.skeletonsCargando = false;
+    }, 2000); //mantener skeletons n seg.
   }
 
 }
