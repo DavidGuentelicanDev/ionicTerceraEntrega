f7b22bfbb026a6bb42753604d14c23aefdd99412
David Guentelican <david.guentelican@gmail.com>
2024-11-12 23:53:47 -0300

    instalado sqlite, mlkit, app nativizada / primeros pasos para iniciar la DB local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 //importacion para APIs
 import { HttpClientModule } from '@angular/common/http';
+//importacion para sqlite
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule], //agregar HttpClieteModule para Apis
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SQLite], //agregar sqlite aca
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+//importacion para sqlite
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbService {
 
-  constructor() { }
+  /* VARIABLES --------------------------------------------------------------------------------- */
+
+  //definir una instancia de la db para poder inicializarla en nulo
+  dbInstancia: SQLiteObject | null = null;
+
+
+  /* CONSTRUCTOR ------------------------------------------------------------------------------- */
+
+  //inyectar dependencias
+  constructor(private sqlite: SQLite) { }
+
 }
